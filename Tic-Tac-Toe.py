6f189ca2d88d9a6e64ba5ae17822908b770d6423
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2021-04-10 22:59:49 +0300

    Editted

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -47,3 +47,44 @@ while(True):
             Player = 1
             
     drawField(currentField)
+
+"""
+
+def drawField(field):
+    print("This is a tic-tac-toe game")
+    for row in range(5):
+        if row%2 == 0:
+            practicalRow = int(row/2)
+            for column in range(5):
+                if column%2 == 0:
+                    practicalColumn = int(column/2)
+                    if column != 4:
+                        print(field[practicalRow][practicalColumn],end="")
+                    else:
+                        print(field[practicalRow][practicalColumn])
+                else:
+                    print("│",end="")
+        else:
+            print("-"*5)
+
+Player = 1
+currentField = [[" "," "," "], [" "," "," "], [" "," "," "]]
+drawField(currentField)
+
+while(True):
+    print("Player's turn: ",Player)
+    moveRow= int(input("Please enter the value of row: "))
+    moveColumn= int(input("\nPlease enter the value of column: "))
+    
+    if Player == 1:
+        if currentField[moveRow][moveColumn] == " ":
+            currentField[moveRow][moveColumn] = "X"
+            Player = 2
+
+    else:
+        if currentField[moveRow][moveColumn] == " ":
+            currentField[moveRow][moveColumn] = "O"
+            Player = 1
+
+    drawField(currentField)
+"""
